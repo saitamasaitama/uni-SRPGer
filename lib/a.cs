1154c394c930f551fd355f75a6e5030a5f38effe
unknown <s-yoshihara@saitama-tec.co.jp>
2019-05-05 04:45:53 +0900

    first

---
+++ lib/a.cs
@@ -0,0 +1,46 @@
+namespace SRPGER
+{
+  public abstract class Field
+  {
+    public FieldTile this[int x]
+    {
+      get => null;
+    }
+  }
+
+  class Field3D
+  {
+
+  }
+
+  public class FieldObject
+  {
+
+  }
+
+  public class FieldTile
+  {
+
+  }
+
+  /*
+  * Levelがたくさん詰まったもの
+   */
+  class Campaign
+  {
+
+  }
+
+  class UnitSet
+  {
+
+  }
+
+  /**
+    Fieldに勝利条件や諸々データを詰め込んだもの
+   */
+  class Level
+  {
+
+  }
+}
